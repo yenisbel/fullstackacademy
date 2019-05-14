83a70eadc8357cc2e3575212452a7db95e165a8e
Yenisbel Valle <yenisbelv@gmail.com>
2019-05-13 22:15:10 -0700

    Solve coercion and truthy excersises with logical operators

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+
+function onlyOne(arg1,arg2,arg3){
+    return ((!!arg1 && !!!arg2 && !!!arg3) || 
+            (!!!arg1 && !!arg2 && !!!arg3) ||
+            (!!!arg1 && !!!arg2 && !!arg3))
+}
+ 
+
--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function zeroDarkThirty(number) {
+    let resultNumber = '';
+    let stringNumber = String(number);
+    if (number === 0) return NaN
+    for (let index = 0; index < stringNumber.length; index++) {
+        const digitNumber = stringNumber[index];
+        if (digitNumber != 0) resultNumber += digitNumber;
+        
+    }
+    return Number(resultNumber);
+}
