d2fc12fd5efd9577eb50afb2641170730d06098b
Yenisbel Valle <yenisbelv@gmail.com>
2019-05-14 21:27:09 -0700

    Solve scope global and functional exec

--- workshops/04-scope/01-fullstack-hq/fullstack-hq.js
+++ workshops/04-scope/01-fullstack-hq/fullstack-hq.js
@@ -1,18 +1,42 @@
 // Edit the code below
+/*let place = 'Planet Earth';
+
+function fullstackHQ(){
+  country = 'United States'
+  return middleFunction();
+
+  function middleFunction() {
+    state = 'New York State';
+    return innerFunction();
+
+    function innerFunction() {
+      city = 'New York City';
+      return innermostFunction();
+
+      function innermostFunction() {
+        return 'Fullstack HQ is at ' + place + ', ' + country + ', ' + state + ', ' + city;
+      }
+    }
+  }
+}
+*/
+
+
+
 let place = 'Planet Earth';
 
 function fullstackHQ(){
   return middleFunction('United States');
 
-  function middleFunction(place) {
+  function middleFunction(country) {
     return innerFunction('New York State');
 
-    function innerFunction(place) {
-      place = 'New York City';
+    function innerFunction(state) {
+      let city = 'New York City';
       return innermostFunction();
 
       function innermostFunction() {
-        return 'Fullstack HQ is at ' + place + ', ' + place + ', ' + place + ', ' + place;
+        return 'Fullstack HQ is at ' + place + ', ' + country + ', ' + state + ', ' + city;
       }
     }
   }
