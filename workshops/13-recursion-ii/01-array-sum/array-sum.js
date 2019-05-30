185b683d67edc54400ccfb5779ca578dc0dc3ace
Yenisbel Valle <yenisbelv@gmail.com>
2019-05-29 18:57:39 -0700

    Solve exercises recursion with nested objects

--- workshops/13-recursion-ii/01-array-sum/array-sum.js
+++ workshops/13-recursion-ii/01-array-sum/array-sum.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function arraySum(arryNumbers) {
+    let suma = 0;
+    // recursive call
+    arryNumbers.forEach(element => {
+        if (Array.isArray(element)){
+            suma += arraySum(element);
+        }else{
+            // case base arr.lenght === 1
+            suma += element;  
+        }   
+    });
+    return suma;
+}
--- workshops/13-recursion-ii/05-search-party/search-party.js
+++ workshops/13-recursion-ii/05-search-party/search-party.js
@@ -1,1 +1,27 @@
-// YOUR CODE BELOW
+
+function searchParty(name, world){
+	for(let place in world){
+        //let value = world[place];
+		if(typeof world[place] === 'string'){
+			let person = world[place];
+			if(person === name){
+				return [place];
+			}
+		}
+		else if(Array.isArray(world[place])){
+			let people = world[place];
+			if(people.includes(name)){
+				return [place];
+			}
+		}
+		else {
+			let newObject = world[place];
+			let objectResult = searchParty(name, newObject);
+			if(objectResult){
+				return [place].concat(objectResult);
+			}
+		}
+	}
+	return null;
+}
+
